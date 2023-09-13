be21ba6ce5bf86ed6459395d4450661837e9ab2b
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2023-09-13 13:38:00 +0200

    exo 8

--- exo.js
+++
@@ -1,4 +0,0 @@
-let price = prompt("Veuillez saisir le prix HT");
-let prix = price * (1 + 0.2);
-let prixTtc = prix - price;
-document.body.innerText = "Le prix TTC est de : " + prixTtc + "€";
---
+++ exo8.js
@@ -0,0 +1,4 @@
+// let price = prompt("Veuillez saisir le prix HT");
+// let prix = price * (1 + 0.2);
+// let prixTtc = prix - price;
+// document.body.innerText = "Le prix TTC est de : " + prixTtc + "€";
