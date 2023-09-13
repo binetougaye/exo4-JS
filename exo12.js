754841341ec8a1031048f513d8fb1b27dfb550cb
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2023-09-13 14:09:43 +0200

    exo 12

---
+++ exo12.js
@@ -0,0 +1,7 @@
+for (i = 1; i < 5; i++) {
+  console.log("boucle" + i);
+  console.log("boucle" + (i += 21));
+  console.log("boucle" + (i += 311));
+  console.log("boucle" + (i += 4111));
+  console.log("boucle" + (i + 51111));
+}
--- exo5.js
+++ exo5.js
@@ -1,4 +1,4 @@
-// Afficher en console tous les multiple de 10
-for (i = 0; i < 1010; i += 10) {
-  console.log("boucle" + i);
-}
+// // Afficher en console tous les multiple de 10
+// for (i = 0; i < 1010; i += 10) {
+//   console.log("boucle" + i);
+// }
